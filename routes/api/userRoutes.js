a296ce1f8e131628f0f17a61098430b10f80547e
GEEZEE91 <GINA.ZIVKOVIC91@GMAIL.COM>
2022-12-18 01:29:08 +0800

    Create user routes for userController

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+//  usersController
+const {
+    getUsers, createUser,
+
+    getSingleUser, updateUser, deleteUser,
+    
+    addFriend, removeFriend,
+  } = require('../../controllers/usersController');
+
+// api/users 
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:id 
+router.route('/:id').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+//  api/users/:userId/friends/:friendId
+router.route('/:id/friends/:friendId').post(addFriend).delete(removeFriend)
+
+
+module.exports = router; 
